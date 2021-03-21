3bda8339d71a99611c48cba2ecebb74f46cb20c2
Chirag-Naik666 <75967533+Chirag-Naik666@users.noreply.github.com>
2021-03-21 10:46:33 +0530

    making tables from sql data

--- analysis.sql
+++ analysis.sql
@@ -1,8 +1,10 @@
 #creating database
+
 Create database analysis;
 use analysis;
 
-#creating table from cricinfo details
+#creating batting table from cricinfo details
+
 create table batting (
 Player varchar(20),
 Matches int,
@@ -19,7 +21,7 @@ zeros int,
 fours int,
 sixes int);
 
-#truncate table batting;
+#inserting values from cricinfo website
 
 insert into batting
 (Player,
@@ -69,4 +71,71 @@ values
 ('Kuldeep Yadav',1,2,0,3,3,1.50,24,12.50,0,0,1,0,0),
 ('S Nadeem',1,2,0,0,0,0.00,25,0.00,0,0,2,0,0);	
 
+#creating bowling table from cricinfo details
+
+create table bowling (
+Player varchar(20),
+Matches int,
+innings int,
+overs float,
+maidens int,
+runs int,
+wickets int,
+best varchar(8),
+Average float,
+economy float,
+strike_rate float,
+fives int,
+tens int);
+
+insert into bowling
+(Player,
+Matches,
+innings,
+overs,
+maidens,
+runs,
+wickets,
+best,
+Average,
+economy,
+strike_rate,
+fives,
+tens)
+values
+('JE Root',4,6,45,7,141,6,'5/8',23.5,3.13,45,1,0),
+('BA Stokes',4,5,42.4,8,153,5,'4/89',30.60,3.58,51.2,0,0),
+('MM Ali',1,2,61.0,10,226,8,'4/98',28.25,3.70,45.7,0,0),
+('DM Bess',2,3,51.0,6,197,5,'4/76',39.40,3.86,61.2,0,0),
+('MJ Leach',4,7,161.0,26,517,18,'4/54',28.72,3.21,53.6,0,0),
+('JC Archer',2,3,35.1,9,122,4,'2/75',30.50,3.46,52.7,0,0),
+('JM Anderson',3,4,65.5,31,127,8,'3/17',15.87,1.92,49.3,0,0),
+('OP Stone',1,2,22.4,6,68,4,'3/47',17.00,3.00,34.0,0,0),
+('Washington Sundar',3,5,38.4,3,130,2,'1/1',65.00,3.36,116.0,0,0),
+('R Ashwin',4,8,188.1,33,471,32,'6/61',14.71,2.50,35.2,3,0),
+('AR Patel',3,6,27.4,27,286,27,'6/38',10.59,2.24,28.3,4,1),
+('Mohammed Siraj',2,4,26.0,7,68,3,'2/45',22.66,2.61,52.0,0,0),
+('I Sharma',4,6,59.0,15,160,6,'2/22',26.66,2.71,59.0,0,0),
+('JJ Bumrah',2,3,48.0,10,129,4,'3/84',32.25,2.68,72.0,0,0),
+('Kuldeep Yadav',1,2,12.2,2,41,2,'2/25',20.50,3.32,37.0,0,0),
+('S Nadeem',1,2,59.0,6,233,4,'2/66',58.25,3.94,88.5,0,0);
+
 select * from batting;
+select * from bowling;
+
+#full join to make combined stat table
+
+create table combined_stat as
+select bt.Player,bt.Matches,bt.runs as runs_scored,bt.best as highest,bt.average as bat_avg,
+hundreds,fifties,bw.overs,bw.runs as runs_given,wickets,bw.best as best_fig,bw.average as bw_avg
+from batting bt
+left join bowling bw
+on bt.Player=bw.Player
+union
+select bt.Player,bt.Matches,bt.runs as runs_scored,bt.best as highest,bt.average as bat_avg,
+hundreds,fifties,bw.overs,bw.runs as runs_given,wickets,bw.best as best_fig,bw.average as bw_avg
+from batting bt
+right join bowling bw
+on bt.Player=bw.Player;
+
+select * from combined_stat;
